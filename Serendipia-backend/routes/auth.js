35859c8caba65edff494fbf0c3be65672b4204fd
juanib99 <juanignaciobisello@hotmail.com>
2025-05-30 16:55:06 -0300

    +

--- Serendipia-backend/routes/auth.js
+++ Serendipia-backend/routes/auth.js
@@ -15,24 +15,38 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/renew', renewToken);
+router.get(
+    '/renew', 
+    renewToken
+);
 
-router.post('/', loginUser );
+router.post('/',
+    [
+        check('email', 'Valid email is required').not().isEmpty(),
+        check('email', 'Invalid email format').isEmail(),
+        check('password', 'Password is required').not().isEmpty(),
+    ], 
+    loginUser 
+);
 
-router.post('/new', [
-    check('name', 'Name is required').not().isEmpty(),
-
-    check('email', 'Invalid email format').isEmail(),
-    check('email', 'Valid email is required').not().isEmpty(),
-
-    check('password', 'Password is required').not().isEmpty(),
-    check('password')
-        .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
-        .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
-        .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
-        .matches(/[0-9]/).withMessage('Password must contain at least one number')
-        .matches(/[\W_]/).withMessage('Password must contain at least one special character'),
-], createUser);
+router.post(
+    '/new', 
+    [
+        check('name', 'Name is required').not().isEmpty(),
+        
+        check('email', 'Invalid email format').isEmail(),
+        check('email', 'Valid email is required').not().isEmpty(),
+        
+        check('password', 'Password is required').not().isEmpty(),
+        check('password')
+            .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
+            .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+            .matches(/[a-z]/).withMessage('Password must contain at least one lowercase letter')
+            .matches(/[0-9]/).withMessage('Password must contain at least one number')
+            .matches(/[\W_]/).withMessage('Password must contain at least one special character'),
+    ],
+    createUser 
+);
 
 
 module.exports = router;
