a1e4384ebee4d0bb368c899de45f258421b21861
juanib99 <juanignaciobisello@hotmail.com>
2025-05-30 23:43:34 -0300

    validation, encripting and login

--- Serendipia-backend/controllers/auth.js
+++ Serendipia-backend/controllers/auth.js
@@ -1,5 +1,7 @@
 const { response } = require("express");
+const bcrypt = require('bcryptjs');
 const User = require('../models/UserModel');
+const { generateJWT } = require("../helpers/jwt");
 // Controlador de autenticación para manejar el registro, inicio de sesión y renovación de token
 // Este controlador se encarga de las operaciones relacionadas con la autenticación de usuarios
 // como el registro, inicio de sesión y renovación de token JWT.
@@ -11,13 +13,34 @@ const createUser = async(req, res = response ) => {
 
     try {
 
-        const user = new User({ name, email, password });
+        // Validar si el usuario ya existe
+        let user  = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario ya existe con ese email'
+            });
+        }
+
+        user = new User({ name, email, password });
+
+        // Encriptar la contraseña
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
+
+        // Guardar el usuario en la base de datos
         await user.save();
 
-        return res.status(201).json({
+        // Generar un token JWT
+        const token = await generateJWT(user.id, user.name);
+        
+        return res.status(200).json({
             ok: true,
-            msg: 'register',
+            uid: user.id,
+            name: user.name,
+            token
         });
+
     } catch (error) {
         res.status(500).json({
             ok: false,
@@ -28,16 +51,47 @@ const createUser = async(req, res = response ) => {
     
 };
 
-const loginUser = (req, res = response) => {
+const loginUser = async(req, res = response) => {
 
     const { email, password } = req.body;
 
-    res.status(200).json({
-        ok: true,
-        msg: 'login',
-        email,
-        password,
-    });
+    try {
+
+        // Validar si el usuario existe
+        let user  = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario o contraseña no son correctos'
+            });
+        }
+
+        // Validar la contraseña
+        const validPassword = bcrypt.compareSync(password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario o contraseña no son correctos'
+            });
+        }
+
+        // Generar un token JWT
+        const token = await generateJWT(user.id, user.name);
+        return res.status(200).json({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+            token
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Error al crear el usuario',
+            error: error.message || 'Error inesperado'
+        });
+    }
+    
 };
 
 const renewToken = (req, res) => {
---
+++ Serendipia-backend/helpers/jwt.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+// Helper para generar un token JWT
+// Este helper se encarga de generar un token JWT para el usuario autenticado
+// Recibe el uid y el nombre del usuario, y devuelve una promesa que resuelve con el token generado
+
+
+const generateJWT = (uid, name) => {
+    return new Promise((resolve, reject) => {
+        const payload = { uid, name };
+        jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: '24h'
+        }, (err, token) => {
+            if (err) {
+                console.error(err);
+                reject('Error generating token');
+            }
+            else {
+                resolve(token);
+            }
+        }
+        );
+    });
+}
+
+module.exports = {
+    generateJWT
+};
